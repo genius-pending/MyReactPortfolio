98fb2e8f98bbcd8f543b87cb0c3a163acaf08228
Shivey Singh <shivey_khangura@hotmail.com>
2021-02-27 12:20:28 +0000

    Updates

---
+++ precache-manifest.b5f059ab443038b7a462c1f7ec86c3ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e44eb956d4cac3a05d8007ff40714839",
+    "url": "/MyReactPortfolio/index.html"
+  },
+  {
+    "revision": "acec5eb1e64c6ec95a0a",
+    "url": "/MyReactPortfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/MyReactPortfolio/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/MyReactPortfolio/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acec5eb1e64c6ec95a0a",
+    "url": "/MyReactPortfolio/static/js/main.4a93329a.chunk.js"
+  },
+  {
+    "revision": "8fb436ff54d1e978f6b6",
+    "url": "/MyReactPortfolio/static/js/runtime-main.6e46c56a.js"
+  }
+]);
